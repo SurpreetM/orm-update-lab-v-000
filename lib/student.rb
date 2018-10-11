ee6e5767df0b0400f168ba31a3a416a71883136a
surpreet.mundra <surpreet.mundra@gmail.com>
2018-10-11 17:25:15 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -36,10 +36,8 @@ class Student
       INSERT INTO students (name, grade)
       VALUES (?, ?)
     SQL
-
     DB[:conn].execute(sql, self.name, self.album)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
-
   end
 
 
